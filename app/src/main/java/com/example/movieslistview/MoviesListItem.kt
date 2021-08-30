7056e7b31877dcdc9230715a0604fb801079e1a5
nadir hamidou <nadir.hamidou96@gmail.com>
2021-08-30 15:54:25 +0100

    Convert java code to Kotlin -still has 2 err

---
+++ app/src/main/java/com/example/movieslistview/MoviesListItem.kt
@@ -0,0 +1,11 @@
+package com.example.movieslistview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoviesListItem : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_list_item)
+    }
+}
